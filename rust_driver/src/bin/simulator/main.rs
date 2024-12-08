0f3adc3f593434f250660548fb83a64b6c3646da
Foreverhighness <Foreverhighness@github.com>
2024-12-08 09:41:54 +0800

    🚧 (test): Refactor testing file generating code.

--- rust_driver/src/bin/simulator/main.rs
+++ rust_driver/src/bin/simulator/main.rs
@@ -23,8 +23,8 @@ fn main() {
     // let mut data = 0;
     // let csr_addr = 0;
     // let word_width = 4;
-    let mut cnt = 0;
-    let mut fragment = 0;
+    let frame = &mut 0;
+    let fragment = &mut 0;
     for _ in 0..100 {
         unsafe {
             rpc.c_netIfcGetRxData(nic_ptr, client_id, 0);
@@ -41,31 +41,13 @@ fn main() {
             continue;
         }
 
-        // generate fragment files
-        {
-            let filename = &format!("fragment-{cnt}-{fragment}.bin");
-            fragment += 1;
-
-            let json = serde_json::to_vec(&nic_payload).unwrap();
-
-            std::fs::write(filename, &json).unwrap();
-            let read_back = std::fs::read(filename).unwrap();
-
-            let value: RpcNetIfcRxTxPayload = serde_json::from_slice(&read_back).unwrap();
-            assert_eq!(value, nic_payload);
-        }
+        generate_frame_fragment_file(&nic_payload, frame, fragment);
 
         let payload = unsafe { core::mem::transmute::<_, [u8; 64]>(nic_payload.data) };
         buffer.extend_from_slice(&payload);
 
         if nic_payload.is_last == 1 {
-            // generate frame files
-            {
-                let filename = &format!("ethernet-frame-{cnt}.bin");
-                cnt += 1;
-                std::fs::write(filename, &buffer).unwrap();
-                assert_eq!(std::fs::read(filename).unwrap(), buffer);
-            }
+            generate_frame_file(&buffer, frame, fragment);
 
             let eth_frame = smoltcp::wire::EthernetFrame::new_checked(buffer.as_slice()).unwrap();
             println!("{}", smoltcp::wire::pretty_print::PrettyPrinter::print(&eth_frame));
@@ -74,4 +56,27 @@ fn main() {
         }
     }
     std::thread::sleep(Duration::from_secs(10));
+}
+
+fn generate_frame_fragment_file(response: &RpcNetIfcRxTxPayload, frame: &mut u32, fragment: &mut u32) {
+    let filename = &format!("fragment-{frame}-{fragment}.bin");
+    *fragment += 1;
+
+    let json = serde_json::to_vec(response).unwrap();
+
+    std::fs::write(filename, &json).unwrap();
+
+    let read_back = std::fs::read(filename).unwrap();
+    let value: RpcNetIfcRxTxPayload = serde_json::from_slice(&read_back).unwrap();
+    assert_eq!(&value, response);
+}
+
+fn generate_frame_file(buffer: &[u8], frame: &mut u32, fragment: &mut u32) {
+    let filename = &format!("ethernet-frame-{frame}.bin");
+    *frame += 1;
+    *fragment = 0;
+
+    std::fs::write(filename, &buffer).unwrap();
+
+    assert_eq!(std::fs::read(filename).unwrap(), buffer);
 }
