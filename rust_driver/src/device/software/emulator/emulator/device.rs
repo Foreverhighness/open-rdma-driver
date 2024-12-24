218439723b0e52a4ea64c2cbc6be6c6a7fa24bff
Foreverhighness <Foreverhighness@github.com>
2024-12-24 22:38:28 +0800

    ✨ (device): New emulator device.

--- rust_driver/src/device/software/emulator/emulator/device.rs
+++ rust_driver/src/device/software/emulator/emulator/device.rs
@@ -1,1 +1,20 @@
+use core::net::IpAddr;
+use std::sync::Arc;
 
+use super::{DmaClient, NetAgent};
+use crate::device::software::emulator::memory_region::Table;
+use crate::device::software::emulator::Emulator;
+
+impl Emulator {
+    pub fn new_emulator(tun_ip: IpAddr) -> Arc<Self> {
+        let dma_client = DmaClient;
+        let mr_table = Table::new();
+
+        let dev = Arc::new(Self::new(dma_client, mr_table));
+
+        dev.start_work_queue();
+        dev.start_net(move |para| NetAgent::new(para.ip.into(), para.subnet_mask.into(), tun_ip));
+
+        dev
+    }
+}
